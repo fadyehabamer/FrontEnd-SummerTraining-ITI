84d21ff5839c4ddbd1b1ba9cd43f0fd5faf72b52
fadyehabamer <fadyamer45@gmail.com>
2021-08-08 01:05:59 +0200

    Add files via upload

---
+++ Day #04/FunctionUsingScript.js
@@ -0,0 +1,9 @@
+// calling all functions in functionsScript.js file 
+
+summition("fady", 2)
+displayFull("Fady", "Ehab", "Amer")
+getFactorialRecursion(5)
+factorialTraditional(5)
+simpleCalc(1, "+", 2)
+gradeCalculation(90)
+getcharcode("fady")
