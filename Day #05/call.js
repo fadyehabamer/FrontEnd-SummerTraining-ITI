b80f9f88c257b3b0330be7e777b3c66a8fe164f5
fadyehabamer <fadyamer45@gmail.com>
2021-08-09 09:11:25 +0200

    Add files via upload

---
+++ Day #05/call.js
@@ -0,0 +1,20 @@
+//  * wordCapitalizer
+wordCapitalizer("fady ehab amer")
+
+// * stringAlphabetical
+sort("fady")
+
+// * longest word in scentence 
+longestWord("Fady Ehab El-Morsy Amer");
+
+// * switch case
+switchCase("FaDy")
+
+// * unique chars
+uniqueCharachters("FadyFadyEhabEhabAmerAmer")
+
+// * random from 0 => 9
+randomVal()
+
+// * Area of Circle
+areaOfCircle(5)
