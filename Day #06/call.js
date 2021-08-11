79e36d334c83ac0c4c315e185321118fd8f721e7
fadyehabamer <fadyamer45@gmail.com>
2021-08-11 09:10:09 +0200

    Add files via upload

---
+++ Day #06/call.js
@@ -0,0 +1,40 @@
+// * 1 - Remove dublicate items from an array
+
+// * using filter
+noDub_filter(my_array);
+
+// * using native algo
+noDub_Native(my_array)
+
+
+// * Sort Ascending
+sort_Ascending(my_array)
+
+// * Ascending sort native
+Ascend_sort_native(my_array)
+
+
+// * sort descending
+sort_descinding(my_array)
+
+// * Descending sort native
+Descend_sort_native(my_array)
+
+// * less than fifty filter
+less_fifty_filter(my_array)
+
+// * less than fifty native
+less_fifty_native(my_array)
+
+// * Min , Max native
+min_max_native(my_array)
+min_max_native_2(my_array)
+
+// * min max MATH
+min_max(my_array)
+
+// * sum and product of array
+sumAll(my_array)
+
+// * Month Name
+monthName(new Date("2000-11-11"))
