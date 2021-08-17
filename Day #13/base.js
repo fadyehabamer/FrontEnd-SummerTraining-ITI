3dcb1170248432141e623b26da69181225c9bb8f
fadyehabamer <fadyamer45@gmail.com>
2021-08-17 22:19:58 +0200

    Add files via upload

---
+++ Day #13/base.js
@@ -0,0 +1,7 @@
+import { rectangle, square } from "./SquaresModule.js";
+import { circle } from "./CircleModule.js"
+
+
+let rec = new rectangle(20, 45);
+let squ = new square(20, 20);
+let cir = new circle(10, 20, 40);
